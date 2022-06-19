f1cdee20f482345af9fef29ce1432ff5ee6f325b
pps <phyophyosan@gmail.com>
2022-06-19 22:04:48 +0630

    edit title

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title') Crud Home Page @endsection
+@section('title') Laravel CRUD Create Page @endsection
 @section('content')
     <div class="container">
         <div class="row justify-content-center my-3">
